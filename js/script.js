9648f3b43340bdcfb5853a9dd2b117b45060c2af
Francis Xavier <75237047+Xavyman2@users.noreply.github.com>
2025-01-24 14:47:18 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ const navUl = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     navUl.classList.toggle('active');
 });
+// Slideshow Code
+document.addEventListener('DOMContentLoaded', function() {
+    const slideshows = document.querySelectorAll('.destination-slideshow');
+
+    slideshows.forEach(slideshow => {
+        const images = slideshow.querySelectorAll('img');
+        let currentImage = 0;
+
+        images[currentImage].style.display = 'block';
+
+        setInterval(() => {
+            images[currentImage].style.display = 'none';
+            currentImage = (currentImage + 1) % images.length;
+            images[currentImage].style.display = 'block';
+        }, 4000);
+    });
+});
